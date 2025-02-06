f5c3d05cf325e79fb195b4a6bb7ce6c022e77db5
salam <chaudharysm00@gmail.com>
2025-02-06 21:27:09 +0545

    store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "../features/userSlice";
+import { userApi } from "../features/api/userApi";
+import { productApi } from "../features/api/productApi";
+import authReducer from "../features/slice/authSlice";
+import cartReducer from "../features/slice/cartSlice";
+import { carouselApi } from "../features/api/carouselApi";
+import { ordersApi } from "../features/api/orderApi";
+import { contactApi } from "../features/api/messageApi";
 
 export const store = configureStore({
   reducer: {
-    user: userReducer,
+    auth: authReducer,
+    cart: cartReducer,
+    [userApi.reducerPath]: userApi.reducer,
+    [contactApi.reducerPath]: contactApi.reducer,
+    [productApi.reducerPath]: productApi.reducer,
+    [ordersApi.reducerPath]: ordersApi.reducer,
+    [carouselApi.reducerPath]: carouselApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(
+      userApi.middleware,
+      productApi.middleware,
+      ordersApi.middleware,
+      carouselApi.middleware,
+      contactApi.middleware
+    ),
 });
