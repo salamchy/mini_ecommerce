405e3313eb766530510a980b5652c68b57bcac51
salam <chaudharysm00@gmail.com>
2025-02-06 21:29:37 +0545

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,51 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setUser } from './features/slice/authSlice';
 import RootLayout from "./components/RootLayout";
 import Home from "./pages/Home";
-import SignIn from "./pages/SignIn";
 import Register from "./pages/Register";
+import Dashboard from "./admin/Dashboard";
+import Login from "./pages/Login";
+import AddProduct from "./pages/products/AddProduct";
+import ProductList from "./pages/products/ProductList";
+import UpdateProduct from "./pages/products/UpdateProduct";
+import AdminList from "./admin/adminComponent/AdminList";
+import ProductDetail from "./pages/products/ProductDetail";
+import CartItems from "./pages/cart/CartItems";
+import CarouselUpload from "./pages/carousel/CarouselUpload";
+import GetCarousel from "./pages/carousel/GetCarousel";
+import Result from "./pages/orders/Result";
+import AboutUs from "./pages/aboutUs/AboutUs";
+import ContactUs from "./pages/contactUs/ContactUs";
+import AdminMessages from "./admin/adminComponent/AdminMessage";
+import UserOrder from "./pages/orders/UserOrder";
+import AllOrders from "./pages/orders/AllOrders";
 
+// Protected Route Wrapper
+const ProtectedRoute = ({ children }) => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  if (!isAuthenticated) {
+    return <Login />;
+  }
+
+  return children;
+};
 
 const App = () => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  useEffect(() => {
+    // Check for user data in local storage
+    const userData = localStorage.getItem('user');
+    if (userData) {
+      const user = JSON.parse(userData);
+      dispatch(setUser({ user, token: user.token }));
+    }
+  }, [dispatch]);
+
   const router = createBrowserRouter([
     {
       path: '/',
@@ -13,29 +53,141 @@ const App = () => {
       children: [
         {
           index: true,
-          element: <Home />
+          element: <Home />,
+        },
+        {
+          path: '/about',
+          element: <AboutUs />,
+        },
+        {
+          path: '/contact',
+          element: <ContactUs />,
+        },
+        {
+          path: '/services',
+          element: <Home />,
+        },
+        {
+          path: '/products',
+          element: <ProductList />,
+        },
+        {
+          path: '/product/:id',
+          element: <ProductDetail />,
+        },
+        {
+          path: '/products/product/:id',
+          element: <ProductDetail />,
+        },
+        {
+          path: '/orders',
+          element: <UserOrder />,
+        },
+        {
+          path: '/cart',
+          element: (
+            <ProtectedRoute>
+              <CartItems />
+            </ProtectedRoute>
+          )
+        },
+        {
+          path: '/result',
+          element: (
+            <ProtectedRoute>
+              <Result />
+            </ProtectedRoute>
+          )
         }
-      ]
+      ],
     },
     {
-      path: '/about',
-      element: <Home />
+      path: '/admin',
+      element: (
+        <ProtectedRoute>
+          <Dashboard />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: '/admin/products/add',
+          element: (
+            <ProtectedRoute>
+              <AddProduct />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: '/admin/products',
+          element: (
+            <ProtectedRoute>
+              <AdminList />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: '/admin/products/update/:id',
+          element: (
+            <ProtectedRoute>
+              <UpdateProduct />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: '/admin/products/detail/:id',
+          element: (
+            <ProtectedRoute>
+              <ProductDetail />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: '/admin/carousel/upload',
+          element: (
+            <ProtectedRoute>
+              <CarouselUpload />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: '/admin/carousel/get-image',
+          element: (
+            <ProtectedRoute>
+              <GetCarousel />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: '/admin/message',
+          element: (
+            <ProtectedRoute>
+              <AdminMessages />
+            </ProtectedRoute>
+          ),
+        },
+        {
+          path: '/admin/all-orders',
+          element: (
+            <ProtectedRoute>
+              <AllOrders />
+            </ProtectedRoute>
+          ),
+        },
+      ],
     },
-    {
-      path: '/services',
-      element: <Home />
-    },
+
     {
       path: '/register',
-      element: <Register />
+      element: <Register />,
     },
     {
       path: '/login',
-      element: <SignIn />
-    }
+      element: <Login />,
+    },
+
   ]);
+
   return <RouterProvider router={router} />;
 }
 
-
-export default App
+export default App;
