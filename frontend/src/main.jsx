736f472857a635d1bb13269cbaddecfa07b2a12d
salam <chaudharysm00@gmail.com>
2025-02-06 21:24:35 +0545

    main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { store } from './app/store.js'
-import { Provider } from 'react-redux'
-import { ToastContainer } from 'react-toastify'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { store } from './app/store.js';
+import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
-    <ToastContainer />
+    <ToastContainer position="top-right" autoClose={2000} />
   </Provider>,
-)
+);
