9d001a5f89edd0b65627e156decc0561b80a6985
salam <chaudharysm00@gmail.com>
2025-02-06 22:41:17 +0545

    createApi

--- frontend/src/features/api/userApi.js
+++ frontend/src/features/api/userApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const userApi = createApi({
   reducerPath: "userApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1/users",
+    baseUrl: "https://mini-ecommerce-1-vrpv.onrender.com/api/v1/users",
     credentials: "include",
   }),
   endpoints: (builder) => ({
