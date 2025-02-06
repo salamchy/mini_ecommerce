d9178df6bb386bd3854e61714badda192077ea3f
salam <chaudharysm00@gmail.com>
2025-02-06 21:27:56 +0545

    git dashboard'

--- frontend/src/admin/Dashboard.jsx
+++ frontend/src/admin/Dashboard.jsx
@@ -1,6 +1,19 @@
+import { Outlet } from "react-router-dom";
+import AdminNav from "./adminComponent/AdminNav";
+import AdminSidebar from "./adminComponent/AdminSidebar";
+
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
-export default Dashboard
+    <div className="flex flex-col h-screen">
+      <AdminNav />
+      <div className="flex flex-1 overflow-hidden">
+        <AdminSidebar className="h-full" />
+        <div className="flex-1 overflow-y-auto p-6">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
